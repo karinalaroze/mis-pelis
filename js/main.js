46367ffd0be670bcb9174fcd9185175390db1212
KarinaLaroze <karinalaroze@gmail.com>
2017-08-10 20:29:19 -0400

    subiendo cambios de la seccion create account

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-$(".button-collapse").sideNav();
-
-$('body').on('click', '.button-collapse', function() {
-    window.alert('too bad :(. This event will never be triggered because the sideNav is stopping propagation.');
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
 });
